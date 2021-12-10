7800b13c313754b619809549db60c2bc335107d2
Luis Viguria <a20121532@pucp.pe>
2021-12-09 19:15:49 -0500

    Initial commit

---
+++ src/test/java/pe/entelgy/reto/RetoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.entelgy.reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
